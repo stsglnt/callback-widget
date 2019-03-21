b399e9cfd553fdf8482f41bb834781115f1fd4d1
Stanislav Galiant <stsglnt@gmail.com>
2019-03-21 19:04:22 +0200

    test inputs

--- src/components/widget-modal/widget-modal.tsx
+++ src/components/widget-modal/widget-modal.tsx
@@ -9,7 +9,34 @@ export class WidgetModal {
   render() {
     return [
       <div class="backdrop"></div>,
-      <div class="modal">Hello, World! I'm future widget modal window </div>
+      <div class="modal">Here goes some dummy text
+        <form>
+          <label htmlFor="">
+            First Name
+            <input/>
+          </label>
+          <label htmlFor="">
+            Last Name
+            <input/>
+          </label>
+          <label htmlFor="">
+            Role
+            <select name="role" id="select_role">
+              <option value="admin">Admin</option>
+              <option value="user">User</option>
+            </select>
+          </label>
+          <label htmlFor="">
+            Phone
+            <input type="number"/>
+          </label>
+          <label htmlFor="">
+            Country
+            <input/>
+          </label>
+        </form>
+        <button type="submit">Submit</button>
+      </div>,
     ];
   }
 }
