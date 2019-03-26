8310c25d36fe88f4a719cd08897c0a607e42440f
Stanislav Galiant <stsglnt@gmail.com>
2019-03-26 13:32:26 +0200

    fixed styles

--- src/components/widget-button/widget-button.tsx
+++ src/components/widget-button/widget-button.tsx
@@ -15,27 +15,35 @@ export class WidgetButton {
     this.isOpenChannels = !this.isOpenChannels;
   }
   render() {
+    const style = {
+      transformOrigin: 'center'
+    }
     return [
       <div class="buttons">
-        <div class="main-button pulse " onClick={this.openChannels.bind(this)}>
-          <img
-            class={this.isOpenChannels ? "d-none" : ''}
-            src="../../assets/icons/call-answer.svg" alt=""/>
-          <span class={!this.isOpenChannels ? "d-none" : 'close-button'}> &#10005;
-          </span>
-        </div>
-        {this.isOpenChannels && (
-        <div class="d-flex">
-          <div class="main-button main-button-phone mln-1">
-            <img
-              src="../../assets/icons/call-answer.svg" alt=""/>
+        <a href="#" id="popup__toggle" onClick={this.openChannels.bind(this)}>
+          <div class={this.isOpenChannels ? "d-none" : "circlephone"} style={style}></div>
+          <div class={this.isOpenChannels ? "d-none" : "circle-fill"} style={style}></div>
+          <div class="img-circle" style={style}>
+            <div style={style} class={this.isOpenChannels ? "d-none" : "img-circleblock"}>
+              <img
+                class={this.isOpenChannels ? "d-none" : ''}
+                src="../../assets/icons/call-answer.svg" alt=""/>
+            </div>
+            <span class={!this.isOpenChannels ? "d-none" : 'close-button'}> &#10005;</span>
           </div>
+        </a>
+        {this.isOpenChannels && (
+        <div class="d-flex channels-wrapper">
           <ul class="channels">
             <li><span>sms</span></li>
             <li><img src="../../assets/icons/telegram.svg" alt=""/></li>
             <li><img src="../../assets/icons/facebook-logo.svg" alt=""/></li>
             <li><img src="../../assets/icons/vk-logo.svg" alt=""/></li>
           </ul>
+          <div class="main-button main-button-phone mln-1">
+            <img
+              src="../../assets/icons/call-answer.svg" alt=""/>
+          </div>
           </div>
         )}
       </div>
