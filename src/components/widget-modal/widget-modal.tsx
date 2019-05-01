8e31f17c3a22948e3a01de5afd215ecc886319a4
Stanislav Galiant <stsglnt@gmail.com>
2019-05-01 14:11:03 +0300

    mask

--- src/components/widget-modal/widget-modal.tsx
+++ src/components/widget-modal/widget-modal.tsx
@@ -1,4 +1,5 @@
-import {Component, Event, EventEmitter, Prop, State} from '@stencil/core';
+import {Component, Event, EventEmitter, Prop, State, Element} from '@stencil/core';
+import IMask from 'imask';
 
 @Component({
   tag: 'widget-modal',
@@ -7,32 +8,43 @@ import {Component, Event, EventEmitter, Prop, State} from '@stencil/core';
 })
 export class WidgetModal {
   @Prop() configs: any;
-  @State() value: string = '+38';
+  @State() value: string;
+  @State() isWorkingTime: string;
+  @State() timer = '00:30';
   @Event() onCloseModal: EventEmitter;
+  @Element() private element: HTMLElement;
   closeModal() {
     this.onCloseModal.emit();
   }
-  handleInputValue(event) {
-    event.preventDefault();
 
-    // if (event.which < 48 || event.which > 57)
-    // {
-    //   console.log('number key');
-    //   event.preventDefault();
-    // }
-    // console.log('event', event);
-    // const inputValue = (event.target as HTMLInputElement).value;
-    // if (this.configs.widget_options.isUkraine) {
-    //   this.value = '+38' + inputValue.substring(3);
-    // } else {
-    //   this.value = (event.target as HTMLInputElement).value;
-    // }
-    // console.log('this value', this.value);
-    // var value = $(this).val();
-    // $(this).val($(this).data("initial") + );
+  handleSubmit() {
+    let seconds = 30;
+    let countdown = setInterval(() => {
+      seconds--;
+      this.timer = `00:${seconds}`;
+      if (seconds <= 0) clearInterval(countdown);
+    }, 1000);
   }
-  handleSubmit(event) {
-    console.log('submit e', event);
+  componentWillLoad(){
+    if (this.configs.widget_options.isUkraine) {
+      this.value = '+38'
+    }
+  }
+  componentDidLoad() {
+    const input = this.element.shadowRoot.querySelector('#phone-input');
+    if (this.configs.widget_options.isUkraine) {
+      const maskOptions = {
+        mask: '+{38}(000)000-00-00'
+      };
+      IMask(input, maskOptions);
+    } else {
+      IMask(input, {
+        mask: Number,
+        signed: false,  // disallow negative
+        min: -10000,
+        max: 10000
+      });
+    }
   }
 
   render() {
@@ -48,21 +60,30 @@ export class WidgetModal {
             <div class="message">Here goes some text bla bla bla bla</div>
           </div>
           <div class="bottom">
-            <form class="form-group" onSubmit={(e) => this.handleSubmit(e)}>
-              <input id="phone-input" class="phone_input" value={this.value} onKeyUp={(e) => {this.handleInputValue(e)}} />
-              <select class="phone_date custom_select">
-                <option value="123">123</option>
-              </select>
-              <select class="phone_hour custom_select">
-                <option value="1243">1243</option>
-              </select>
-              <select class="phone_minute custom_select">
-                <option value="00" selected>00</option>
-                <option value="15">15</option>
-                <option value="30">30</option>
-                <option value="45">45</option>
-              </select>
+            <form class="form-group" onSubmit={() => this.handleSubmit()}>
+              <input id="phone-input" class="phone_input" value={this.value}/>
+              {this.isWorkingTime && (
+                <select class="phone_date custom_select">
+                  <option value="123">123</option>
+                </select>
+              )}
+              {this.isWorkingTime && (
+                <select class="phone_hour custom_select">
+                  <option value="1243">1243</option>
+                </select>
+              )}
+              {this.isWorkingTime && (
+                <select class="phone_minute custom_select">
+                  <option value="00" selected>00</option>
+                  <option value="15">15</option>
+                  <option value="30">30</option>
+                  <option value="45">45</option>
+                </select>
+              )}
               <button type="submit" class="submit_button">Waiting for the call</button>
+              {!this.isWorkingTime && (
+                <span>{this.timer}</span>
+              )}
             </form>
           </div>
         </div>
